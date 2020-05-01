0d7059d5e6da2705400caa46f9fe1629c21d638e
Chance Strickland <hi@chancedigital.io>
2020-05-01 00:35:44 -0700

    add types for requestIdleCallback

---
+++ types/index.d.ts
@@ -0,0 +1,19 @@
+type RequestIdleCallbackOptions = {
+  timeout: number;
+};
+type RequestIdleCallbackDeadline = {
+  readonly didTimeout: boolean;
+  timeRemaining: () => number;
+};
+
+declare global {
+  interface Window {
+    requestIdleCallback: (
+      callback: (deadline: RequestIdleCallbackDeadline) => void,
+      opts?: RequestIdleCallbackOptions
+    ) => number;
+    cancelIdleCallback: (handle: number) => void;
+  }
+}
+
+export {}
