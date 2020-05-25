357f760f6b7bf9a89f0b1f506a21b07514a69fd9
Rauno Freiberg <freiberggg@gmail.com>
2020-05-25 16:59:35 +0300

    feat: asynchronously import axe-core (#13)
    * feat: asynchronously import axe-core
    
    * fix: only import axe-core if not disabled
    
    * config updates
    
    * lazy load default export in dev only
    
    * fix: create Loader component for entry point
    
    Co-authored-by: Chance Strickland <hi@chancedigital.io>

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,11 +1,18 @@
 const postcss = require('rollup-plugin-postcss');
+const path = require('path');
 
 module.exports = {
-	rollup(config, options) {
-		config.plugins = [
-			postcss(),
-			...config.plugins,
-		];
-		return config
-	},
+  rollup(config, options) {
+    config.plugins = [postcss(), ...config.plugins];
+
+    // To get code splitting to work, we need to set output.dir instead of
+    // output.file, which is what TSDX does by default
+    const { file, ...output } = config.output || {};
+    config.output = {
+      ...output,
+      dir: path.join(__dirname, 'dist'),
+    };
+
+    return config;
+  },
 };
